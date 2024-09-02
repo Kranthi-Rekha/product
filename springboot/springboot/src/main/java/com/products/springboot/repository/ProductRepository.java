0fb70b89d967462c705b48cce1e4e1dff4f399db
CHERUKU KRANTHI REKHA <123478117+Kranthi-Rekha@users.noreply.github.com>
2024-09-02 17:36:41 +0530

    Delete springboot directory

--- springboot/springboot/src/main/java/com/products/springboot/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.products.springboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.products.springboot.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long>{
-
-}
